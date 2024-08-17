c1a979746c45285adab65f7ac18779afe7ef4466
rlitoncs <rj_liton@hotmail.com>
2024-08-16 23:06:52 -0400

    Feat: Add heart button

--- frontend/src/components/PhotoFavButton.jsx
+++ frontend/src/components/PhotoFavButton.jsx
@@ -4,10 +4,18 @@ import FavIcon from './FavIcon';
 import '../styles/PhotoFavButton.scss';
 
 function PhotoFavButton() {
+  const [click, setClick] = useState(false);
+
+  const handleClick = () => {
+    console.log(click);
+    return !click ? setClick(true) : setClick(false);
+  }
+
   return (
     <div className="photo-list__fav-icon">
-      <div className="photo-list__fav-icon-svg">
+      <div className="photo-list__fav-icon-svg"  onClick={handleClick}>
         {/* Insert React */}
+        <FavIcon />
       </div>
     </div>
   );
--- frontend/src/components/PhotoListItem.jsx
+++ frontend/src/components/PhotoListItem.jsx
@@ -1,12 +1,14 @@
 import React from "react";
 
 import "../styles/PhotoListItem.scss";
+import PhotoFavButton from './PhotoFavButton'
 
 const PhotoListItem = (props) => {
   /* Insert React */
 
   return (
     <div key={props.photo.id} className="photo-list__item">
+      <PhotoFavButton/>
       <img className="photo-list__image" src={props.photo.imageSource}></img>
       <div className="photo-list__user-details">
         <img className="photo-list__user-profile" src={props.photo.profile}></img>
