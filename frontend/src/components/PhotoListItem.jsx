e514d0661bf0575034615c4181728c6819de5f31
rlitoncs <rj_liton@hotmail.com>
2024-08-16 22:33:10 -0400

    Fix: add styling to photo list item

--- frontend/src/components/PhotoListItem.jsx
+++ frontend/src/components/PhotoListItem.jsx
@@ -10,8 +10,12 @@ const PhotoListItem = (props) => {
       <img className="photo-list__image" src={props.photo.imageSource}></img>
       <div className="photo-list__user-details">
         <img className="photo-list__user-profile" src={props.photo.profile}></img>
-        <div className="photo-list__user-info" >{props.photo.username}</div>
-        <div className="photo-list__user-location">{props.photo.location.city} {props.photo.location.country}</div>
+        <div className="photo-list__user-info" >
+          {props.photo.username}
+          <div className="photo-list__user-location">
+            {props.photo.location.city}, {props.photo.location.country}
+          </div>
+        </div>
       </div>
     </div>
   );
