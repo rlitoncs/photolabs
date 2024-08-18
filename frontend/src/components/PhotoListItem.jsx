1782c07e0771cbe90c66a9cbe01f228c2c6985bc
rlitoncs <rj_liton@hotmail.com>
2024-08-17 21:23:42 -0400

    Fix: refactor photolistitem

--- frontend/src/components/PhotoListItem.jsx
+++ frontend/src/components/PhotoListItem.jsx
@@ -10,11 +10,11 @@ const PhotoListItem = (props) => {
     
     <div key={props.photo.id} className="photo-list__item">
       <PhotoFavButton id={props.photo.id}/>
-      <img className="photo-list__image" src={props.photo.imageSource}></img>
+      <img className="photo-list__image" src={props.photo.urls.regular}></img>
       <div className="photo-list__user-details">
-        <img className="photo-list__user-profile" src={props.photo.profile}></img>
+        <img className="photo-list__user-profile" src={props.photo.user.profile}></img>
         <div className="photo-list__user-info" >
-          {props.photo.username}
+          {props.photo.user.username}
           <div className="photo-list__user-location">
             {props.photo.location.city}, {props.photo.location.country}
           </div>
