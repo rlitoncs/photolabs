4d8013dd86a2505b9d9f9f36b17efddc80d58f28
rlitoncs <rj_liton@hotmail.com>
2024-08-18 01:27:46 -0400

    Feat: display notification for favourite photos

--- frontend/src/routes/HomeRoute.jsx
+++ frontend/src/routes/HomeRoute.jsx
@@ -6,24 +6,31 @@ import {useState} from 'react';
 
 const HomeRoute = (props) => {
   const [favourites, setFavourites] = useState([]);
-
+  console.log(favourites);
   const isFavourite = (photo_id) => {
     return favourites.includes(photo_id);
   }  
 
   const toggleFavourites = (photo_id) => {
-    setFavourites(favourites.push(photo_id));
+    if (favourites.includes(photo_id)) {
+      setFavourites(favourites.filter(id => id !== photo_id));
+    } else {
+      setFavourites([...favourites, photo_id]);
+    }
   }
-
+  
+  const isFavPhotoExist = favourites.length > 0;
 
   return (
     <div className="home-route"> 
-      <TopNavigationBar topics={props.topics}/>
+      <TopNavigationBar 
+        topics={props.topics}
+        isFavPhotoExist={isFavPhotoExist}
+        />
       <PhotoList 
         photos={props.photos}
         isFavourite={isFavourite}
         toggleFavourites={toggleFavourites}
-      
       />
     </div>
   );
