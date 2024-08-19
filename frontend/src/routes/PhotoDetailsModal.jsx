7bbb3204c68500b1d4f240db78d9e1016ae34bb5
rlitoncs <rj_liton@hotmail.com>
2024-08-19 14:55:39 -0400

    Feat: Display image and user on modal

--- frontend/src/routes/PhotoDetailsModal.jsx
+++ frontend/src/routes/PhotoDetailsModal.jsx
@@ -1,15 +1,29 @@
 import React, { useState } from 'react';
-
 import '../styles/PhotoDetailsModal.scss'
 import closeSymbol from '../assets/closeSymbol.svg';
+import PhotoList from 'components/PhotoList';
+
 
 const PhotoDetailsModal = (props) => {
-  console.log(props.photo);
+
   return (
-    <div className={"photo-details-modal"}>
+    <div className="photo-details-modal">
       <button className="photo-details-modal__close-button" onClick={props.handleDisplayModal}>
         <img src={closeSymbol} alt="close symbol"/>
       </button>
+      <div className="photo-details-modal__image"> 
+        <img src={props.photo.urls.full} />
+      </div>
+
+      <div className="photo-details-modal__header"> 
+        <img className="photo-list__user-profile" src={props.photo.user.profile}></img>
+        <div className="photo-list__user-info" >
+          {props.photo.user.username}
+          <div className="photo-list__user-location">
+            {props.photo.location.city}, {props.photo.location.country}
+          </div>
+        </div>
+      </div>
     </div>
   )
 };
